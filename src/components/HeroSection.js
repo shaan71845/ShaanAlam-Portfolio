9b5739c40d4ea21804a8fcd5e2231b7b4a155f70
Shaan Alam <shaanalam369@gmail.com>
2021-04-01 14:34:46 +0530

    changed the layout of hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,40 +1,79 @@
 import Navbar from "./Navbar";
 import Fade from "react-reveal/Fade";
 
-const HeroSection = () => (
-  <>
-    <Navbar />
-    <section className="home" id="home">
-      <div className="container">
-        <Fade bottom>
-          <img src="/images/me.png" alt="Shaan Alam" />
-        </Fade>
-        <Fade bottom>
-          <h1 className="home__myName">Shaan Alam</h1>
-        </Fade>
-        <Fade bottom>
-          <p>
-            Hi there 👋️, I'm Shaan Alam. I am a Front-end Developer. I like to
-            create amazing stuffs using JavaScript, ReactJS and Firebase. See
-            some of my Projects below.
-          </p>
-        </Fade>
-        <Fade bottom>
-          <div className="home__actions">
-            <a href="#projects" className="btn__primary">
-              Projects
-            </a>
-            <a
-              href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
-              className="btn__primary"
-            >
-              Download CV
-            </a>
-          </div>
-        </Fade>
-      </div>
-    </section>
-  </>
-);
+// const HeroSection = () => (
+//   <>
+//     <Navbar />
+//     <section className="home" id="home">
+//       <div className="container">
+//         <Fade bottom>
+//           <img src="/images/me.png" alt="Shaan Alam" />
+//         </Fade>
+//         <Fade bottom>
+//           <h1 className="home__myName">Shaan Alam</h1>
+//         </Fade>
+//         <Fade bottom>
+//           <p>
+//             Hi there 👋️, I'm Shaan Alam. I am a Front-end Developer. I like to
+//             create amazing stuffs using JavaScript, ReactJS and Firebase. See
+//             some of my Projects below.
+//           </p>
+//         </Fade>
+//         <Fade bottom>
+//           <div className="home__actions">
+//             <a href="#projects" className="btn__primary">
+//               Projects
+//             </a>
+//             <a
+//               href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
+//               className="btn__primary"
+//             >
+//               Download CV
+//             </a>
+//           </div>
+//         </Fade>
+//       </div>
+//     </section>
+//   </>
+// );
+
+const HeroSection = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero__section" id="home">
+        <div className="container">
+          <header>
+            <Fade>
+              <h4>Hi there 👋️, I'm</h4>
+            </Fade>
+            <Fade bottom>
+              <h1>Shaan Alam</h1>
+            </Fade>
+            <Fade bottom>
+              <p>
+                I am a Front-end Developer. I like to create amazing stuffs
+                using JavaScript. See some of my projects below!!
+              </p>
+            </Fade>
+          </header>
+          <Fade top>
+            <div className="hero__actions">
+              <a href="#projects" className="btn__primary">
+                Projects
+              </a>
+              <a
+                href="https://github.com/shaan71845/python_notes/files/6216889/CV.Shaan.Alam.pdf"
+                className="btn__primary"
+              >
+                Download CV
+              </a>
+            </div>
+          </Fade>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default HeroSection;
