3f6221a7317875736e132e381c960b0971a3d2da
Shaan Alam <shaanalam369@gmail.com>
2021-04-02 11:49:35 +0530

    added animation to skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,39 +1,55 @@
+import Fade from "react-reveal/Fade";
+
 const Skills = () => {
   return (
     <section className="skills__section">
-      <h1>Skills and Tools</h1>
+      <Fade top>
+        <h1>Skills and Tools</h1>
+      </Fade>
       <div className="container">
         <div className="skills__leftContainer">
-          <div className="skill">
-            <h4>Markup/Styling languages</h4>
-            <img src="/images/html.svg" alt="HTML5" />
-            <img src="/images/css.svg" alt="CSS3" />
-          </div>
-          <div className="skill">
-            <h4>Programming Languages</h4>
-            <img src="/images/javascript.svg" alt="JavaScript" />
-            <img src="/images/python.svg" alt="Python" />
-          </div>
-          <div className="skill">
-            <h4>Web frameworks/libraries</h4>
-            <img src="/images/react.svg" alt="ReactJS" />
-            <img src="/images/firebase.svg" alt="Firebase"/>
-          </div>
+          <Fade bottom>
+            <div className="skill">
+              <h4>Markup/Styling languages</h4>
+              <img src="/images/html.svg" alt="HTML5" />
+              <img src="/images/css.svg" alt="CSS3" />
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="skill">
+              <h4>Programming Languages</h4>
+              <img src="/images/javascript.svg" alt="JavaScript" />
+              <img src="/images/python.svg" alt="Python" />
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="skill">
+              <h4>Web frameworks/libraries</h4>
+              <img src="/images/react.svg" alt="ReactJS" />
+              <img src="/images/firebase.svg" alt="Firebase" />
+            </div>
+          </Fade>
         </div>
 
         <div className="skill__rightContainer">
-          <div className="skill">
-            <h4>State Management</h4>
-            <img src="/images/redux.svg" alt="Redux" />
-          </div>
-          <div className="skill">
-            <h4>Version Control System</h4>
-            <img src="/images/git.svg" alt="Git" />
-          </div>
-          <div className="skill">
-            <h4>Design tools</h4>
-            <img src="/images/figma.svg" alt="Figma" />
-          </div>
+          <Fade bottom>
+            <div className="skill">
+              <h4>State Management</h4>
+              <img src="/images/redux.svg" alt="Redux" />
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="skill">
+              <h4>Version Control System</h4>
+              <img src="/images/git.svg" alt="Git" />
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="skill">
+              <h4>Design tools</h4>
+              <img src="/images/figma.svg" alt="Figma" />
+            </div>
+          </Fade>
         </div>
       </div>
     </section>
