648fc13fe73965c58ff2d94759cdaad1652f932d
Shaan Alam <shaanalam369@gmail.com>
2021-06-09 08:12:00 +0530

    completed: [id].js

--- components/CodeSnippet/index.js
+++ components/CodeSnippet/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import bash from "react-syntax-highlighter/dist/cjs/languages/hljs/bash";
 import javascript from "react-syntax-highlighter/dist/cjs/languages/hljs/javascript";
@@ -5,12 +6,27 @@ import synthwave84 from "react-syntax-highlighter/dist/cjs/styles/prism/synthwav
 import styled from "styled-components";
 
 const CodeSnippet = ({ code, language }) => {
+  const [copyText, setCopyText] = useState("Copy");
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(code);
+
+    setCopyText("Copied");
+  };
+
   return (
     <Container>
       <Titlebar>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
+        <div className="titlebar-actions">
+          <div className="circle"></div>
+          <div className="circle"></div>
+          <div className="circle"></div>
+        </div>
+        <div className="copy-to-clipboard">
+          <a href="#!" onClick={copyToClipboard}>
+            {copyText}
+          </a>
+        </div>
       </Titlebar>
       <SyntaxHighlighter
         language={language}
@@ -19,7 +35,7 @@ const CodeSnippet = ({ code, language }) => {
           margin: "2em 0",
           fontFamily: "Fira Code, monospace",
           width: "100%",
-          fontSize: "1.2em",
+          fontSize: "1em",
           margin: "auto",
           background: "transparent",
         }}
@@ -45,27 +61,48 @@ const Titlebar = styled.div`
   top: 1em;
   left: 1em;
   display: flex;
-  width: 100%;
+  justify-content: space-between;
+  width: 95%;
 
-  .circle {
-    height: 14px;
-    width: 13px;
-    border-radius: 100%;
-    margin-right: 10px;
+  .titlebar-actions {
+    display: flex;
 
-    &:nth-child(1) {
-      background: #ff5f56;
-      border: 1px solid #e0443e;
+    .circle {
+      height: 14px;
+      width: 13px;
+      border-radius: 100%;
+      margin-right: 10px;
+
+      &:nth-child(1) {
+        background: #ff5f56;
+        border: 1px solid #e0443e;
+      }
+
+      &:nth-child(2) {
+        background: #ffbd2e;
+        border: 1px solid #dea123;
+      }
+
+      &:nth-child(3) {
+        background: #27c93f;
+        border: 1px solid #1aab29;
+      }
     }
+  }
 
-    &:nth-child(2) {
-      background: #ffbd2e;
-      border: 1px solid #dea123;
-    }
+  .copy-to-clipboard {
+    a {
+      font-size: 0.8em;
+      font-family: "Fira Code", monospace;
+      text-decoration: none;
+      padding: 0.5em 1em;
+      transition: all 0.5s;
+      background: rgba(0, 0, 0, 0.2);
+      border-radius: 8px;
 
-    &:nth-child(3) {
-      background: #27c93f;
-      border: 1px solid #1aab29;
+      &:hover {
+        background: rgba(0, 0, 0, 0.5);
+      }
     }
   }
 `;
